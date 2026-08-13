a00862fd2fc423ebcdb5eb2f5209d4e8b55c4518
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-08-13 18:13:43 +0530

    feat: remove unnecessary gradient overlay from InvestigationCharacter component

--- components/dashboard/hero/investigation-character.tsx
+++ components/dashboard/hero/investigation-character.tsx
@@ -44,7 +44,6 @@ export function InvestigationCharacter({ containerRef }: InvestigationCharacterP
           sizes="46vw"
           className="sr-dash-character sr-hero-character-mask object-contain object-bottom"
         />
-        <div className="absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-(--sr-bg-elevated) to-transparent" />
       </div>
     </div>
   );
